7da50543074010c27b601c9fb9a0c25de2527c08
chris b <christopherbrowning@Christophers-MacBook-Pro.local>
2023-07-20 21:43:43 +0200

    complete first JavaScript challenge

--- script.js
+++ script.js
@@ -1,6 +1,112 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+// Varibales
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jones");
+// console.log(23)
 
-40 + 8 + 23 - 10;
+/*let firstName = "Jones";
+console.log(firstName);
 
-console.log(40 + 8 + 23 - 10);
+let chris_brown = "singer";
+let $function = 27;
+
+let person = "Chris Brown"
+
+Data types Interger, number & string
+let javascritpIsFun = true;
+console.log(typeof true);
+console.log(typeof javascritpIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jones');
+
+javascritpIsFun = 'YES!';
+console.log(typeof javascritpIsFun);
+
+// Undefined
+let year;
+console.log(year)
+console.log(typeof year);
+// Dynamtic typing to update year as a number
+year = 1991;
+console.log(year);
+
+console.log(null);
+
+
+// Declare varibales
+// Mutate a varibal use LET
+let age = 30;
+age = 31;
+
+// Can not mutate COSNT varibales
+const birthYear = 1991;
+birthYear = 1990;
+
+// Never use VAR, it's old school!
+var job = "programmer";
+job = "teacher"
+
+
+// Basic operators
+// Math operators
+const ageChris = 2027 - 1991;
+const ageVika = 2027 - 2018;
+console.log(ageChris, ageVika);
+
+const now = 2027
+const ageMike = now - 1991;
+const agePeter = now - 2018;
+console.log(ageMike, agePeter);
+
+console.log(ageMike * 2, agePeter / 10, 2 ** 3)
+
+const firstName = 'Chris';
+const lastName = 'Brown';
+console.log(firstName + ' ' + lastName);
+
+
+let x = 10 + 5;
+console.log(x);
+
+// Assingment operators
+z += 10; // z = x + 10 = 25
+z *= 4; // z = z * 4 = 100
+z++; // z = z = 1
+z--;
+z--;
+console.log(z);
+
+
+// Comparison operators
+const ageChris = 2027 - 1991;
+const ageVika = 2027 - 2018;
+console.log(ageChris, ageVika);
+
+console.log(ageChris > ageVika); // >, <, >=, <=
+console.log(ageChris >= 18);
+
+// Operator procedance
+const now = 2027
+const ageChris = 2027 - 1991;
+const ageVika = 2027 - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+*/
+
+// Coding challenge
+// Use formular to work out BMI's
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJonh = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (massJohn * heightMark);
+// Compare if mark is higher than John
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
